cb63ccb376f7721b44b820c3234634e612f30b95
chenrf <chenrf.dev@gmail.com>
2023-01-25 11:30:09 +0800

    fix: modify vite config for aws-sdk in browser support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
   plugins: [vue({
     template: {
       compilerOptions: {
